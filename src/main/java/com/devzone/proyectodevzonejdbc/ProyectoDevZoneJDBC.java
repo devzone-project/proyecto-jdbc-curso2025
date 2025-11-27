0b9cbac75882652782af12027a6984d975a84ea8
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-11-26 21:43:15 -0600

    Se ha inicializado el repositorio de la primera parte del curso JAVA JEE donde se abarcara JDBC

---
+++ src/main/java/com/devzone/proyectodevzonejdbc/ProyectoDevZoneJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.devzone.proyectodevzonejdbc;
+
+/**
+ *
+ * @author pablo03
+ */
+public class ProyectoDevZoneJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
